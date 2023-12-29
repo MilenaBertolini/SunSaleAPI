43a8ca773c9071b72b4816a94ca5359a282af1f1
Rodrigo Machado <rodrigoborgesmachado@gmail.com>
2023-12-29 15:08:55 -0300

    Fazendo alterçaão para enviar o email com a meta.

--- Application/Implementation/Services/MetasService.cs
+++ Application/Implementation/Services/MetasService.cs
@@ -32,7 +32,11 @@ namespace Application.Implementation.Services
             entity.Updated = DateTime.Now;
             entity.IsActive = "1";
 
-            return await _repository.Add(entity);
+            var retorno =  await _repository.Add(entity);
+            await EnviaEmailInicial(retorno);
+
+            return retorno;
+
         }
 
         public Task<bool> DeleteById(int id)
@@ -92,9 +96,22 @@ namespace Application.Implementation.Services
             builder.AppendLine("      <td style=\"padding: 20px; text-align: left;\">");
             builder.AppendLine($"		<h1 style=\"color: #333333;\">Meta para {main.DataObjetivo.ToString("dd/MM/yyyy")}:</h1>");
             builder.AppendLine("		<ul>");
-            builder.AppendLine("			<li>");
-            builder.AppendLine($"				<h2 style=\"color: #333333;\">{main.Meta}</h2>");
-            builder.AppendLine("			</li>");
+            if (!main.Meta.Contains("|"))
+            {
+                builder.AppendLine("			<li>");
+                builder.AppendLine($"				<h2 style=\"color: #333333;\">{main.Meta}</h2>");
+                builder.AppendLine("			</li>");
+            }
+            else
+            {
+                var split = main.Meta.Split('|');
+                foreach (var item in split)
+                {
+                    builder.AppendLine("			<li>");
+                    builder.AppendLine($"				<h2 style=\"color: #333333;\">{item}</h2>");
+                    builder.AppendLine("			</li>");
+                }
+            }
             builder.AppendLine("		</ul>");
             builder.AppendLine($"		<h3 style=\"color: #333333;\">Esperamos que tenha concluído!</h>");
             builder.AppendLine("        <br>");
@@ -110,6 +127,69 @@ namespace Application.Implementation.Services
 
             await _serviceEmail.Add(_mapper.Map<Email>(new EmailViewModel()
             { 
+                Assunto = "Atualização de meta para " + main.DataObjetivo.ToString("dd/MM/yyyy"),
+                Destinatario = main.Email,
+                Texto = builder.ToString()
+            }));
+        }
+
+        private async Task EnviaEmailInicial(Main main)
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine("<!DOCTYPE html>");
+            builder.AppendLine("<html lang=\"pt-br\">");
+            builder.AppendLine("");
+            builder.AppendLine("<head>");
+            builder.AppendLine("  <meta charset=\"UTF-8\">");
+            builder.AppendLine("  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+            builder.AppendLine("  <title>Atualizações no Projeto MetasTrack</title>");
+            builder.AppendLine("</head>");
+            builder.AppendLine("");
+            builder.AppendLine("<body style=\"font-family: Arial, sans-serif; margin: 0; padding: 0; background-color: #f4f4f4;\">");
+            builder.AppendLine("");
+            builder.AppendLine("  <table align=\"center\" cellpadding=\"0\" cellspacing=\"0\" width=\"800\" style=\"border-collapse: collapse; background-color: #ffffff; margin-top: 20px; margin-bottom: 20px;\">");
+            builder.AppendLine("    <tr>");
+            builder.AppendLine("      <td style=\"padding: 20px; text-align: center;\">");
+            builder.AppendLine("        <img src=\"https://www.sunsalesystem.com.br/img/logo.png\" alt=\"SunSale System Logo\" style=\"display: block; margin: auto;\" width=\"20%\" height=\"20%\"> SunSale System - MetasTrack");
+            builder.AppendLine("      </td>");
+            builder.AppendLine("    </tr>");
+            builder.AppendLine("    <tr>");
+            builder.AppendLine("      <td style=\"padding: 20px; text-align: left;\">");
+            builder.AppendLine($"		<h1 style=\"color: #333333;\">Meta recebida!</h1>");
+            builder.AppendLine("		<ul>");
+            if (!main.Meta.Contains("|"))
+            {
+                builder.AppendLine($"		<h2 style=\"color: #333333;\">Acaba de ser feito o cadastro para a meta em {main.DataObjetivo.ToString("dd/MM/yyyy")}:</h1>");
+                builder.AppendLine("			<li>");
+                builder.AppendLine($"				<h2 style=\"color: #333333;\">{main.Meta}</h2>");
+                builder.AppendLine("			</li>");
+            }
+            else
+            {
+                builder.AppendLine($"		<h2 style=\"color: #333333;\">Acaba de ser feito o cadastro para as metas em {main.DataObjetivo.ToString("dd/MM/yyyy")}:</h1>");
+                var split = main.Meta.Split('|');
+                foreach (var item in split)
+                {
+                    builder.AppendLine("			<li>");
+                    builder.AppendLine($"				<h2 style=\"color: #333333;\">{item}</h2>");
+                    builder.AppendLine("			</li>");
+                }
+            }
+            builder.AppendLine("		</ul>");
+            builder.AppendLine($"		<h3 style=\"color: #333333;\">Esperamos que conclua!</h>");
+            builder.AppendLine("        <br>");
+            builder.AppendLine("        <p style=\"color: #555555;\">Atenciosamente,</p>");
+            builder.AppendLine("        <p style=\"color: #555555;\">SunSale System</p>");
+            builder.AppendLine("      </td>");
+            builder.AppendLine("    </tr>");
+            builder.AppendLine("  </table>");
+            builder.AppendLine("");
+            builder.AppendLine("</body>");
+            builder.AppendLine("");
+            builder.AppendLine("</html>");
+
+            await _serviceEmail.Add(_mapper.Map<Email>(new EmailViewModel()
+            {
                 Assunto = "Atualização de meta para " + main.DataObjetivo.ToString("dd/MM/yyyy"),
                 Destinatario = main.Email,
                 Texto = builder.ToString()
